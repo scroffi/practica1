09fca523ce111451e7c1be91b4d21668069606ca
usuario <usuario@usuario>
2019-12-17 07:52:59 -0500

    Feliz navidad y prospero año nuevo

---
+++ src/practicab/unapractica.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicab;
+
+/**
+ *
+ * @author usuario
+ */
+public class unapractica {
+    public static void main(String[] args) {
+        practica p =new practica();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+    }
+}
